0062bc41386c20b66887cd09fec6fd4c9e859e54
sebbor2 <sebbor2@gmail.com>
2018-01-10 16:11:48 +0100

    projet avec service et mockArticles

--- load.php
+++ load.php
@@ -1,21 +1,2 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-
-$conn = mysqli_connect("localhost","seb","rXlr98@6","bordeu_blog","3306");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT * FROM `blog_article` WHERE 1";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Chargement OK";
-}
-else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
 ?>
